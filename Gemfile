740ffd4089bbcd8f74d400074b12b0841c5686e4
Alexei Danchenkov <adanchenkov@emailline.net>
2023-09-24 13:44:00 -0700

    upgrade with changes from sprockets branch

--- Gemfile
+++ Gemfile
@@ -54,6 +54,8 @@ gem "dartsass-rails", "~> 0.5.0"
 
 gem "bootstrap", "~> 5.3"
 
+gem "devise", "~> 4.9"
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[mri mingw x64_mingw]
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,5 +5,16 @@ import "controllers"
 import "popper"
 import "bootstrap"
 
-const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
-const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+// Note that this does not work with turbo reloading
+
+document.addEventListener("turbo:load", function () {
+	const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+	const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+	popoverList
+	// let frame = document.getElementById("messagesFrame");
+	// if (frame.complete) {
+	//  frame.scrollTo(0, frame.scrollHeight)
+	// } else {
+	//   frame.loaded.then(() => frame.scrollTo(0, frame.scrollHeight))
+	// }
+})
