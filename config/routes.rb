afed2fff82084c5acf25d9577c541211b519d9db
Alexei Danchenkov <adanchenkov@emailline.net>
2023-09-28 07:26:11 -0700

    tmp

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,19 @@
 Rails.application.routes.draw do
-  get 'users/profile'
+  get "users/profile"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # resources :about, only: [:index]
-  devise_for :users, controllers: {
-      sessions: 'users/sessions',
-      registrations: 'users/registrations'
-    }
-  get '/u/:id', to: "users#profile", as: 'user'
+  devise_for :users,
+             controllers: {
+               sessions: "users/sessions",
+               registrations: "users/registrations"
+             }
+  get "/u/:id", to: "users#profile", as: "user"
 
   get :about, to: "about#index"
-  resources :posts
+
+  resources :posts do
+    resources :comments
+  end
 
   # Defines the root path route ("/")
   root "about#home"
